fc133dd4a834db5212c79ead78b8d02a9c9c021f
Haya-Faisal <hf2434@nyu.edu>
2026-03-30 18:30:21 +0800

    port

--- portrait-in-the-air/public/sky.js
+++ portrait-in-the-air/public/sky.js
@@ -7,7 +7,7 @@ if (
   location.hostname.toLowerCase().startsWith("browsercircus") ||
   location.hostname.toLowerCase().startsWith("www")
 ) {
-  socket = io({ path: "/lesley/port-4290/socket.io" });
+  socket = io({ path: "/haya/port-4230/socket.io" });
 } else {
   socket = io();
 }
