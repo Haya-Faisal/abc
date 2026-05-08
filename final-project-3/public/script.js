e266f12f86cb4e81411a29a6d41b4167b73f9f8c
Haya-Faisal <hf2434@nyu.edu>
2026-05-08 16:37:25 +0800

    bg

--- final-project-3/public/script.js
+++ final-project-3/public/script.js
@@ -428,7 +428,7 @@ function setup() {
 // p5 DRAW
 // ═══════════════════════════════════════════════════════════════════════════
 function draw() {
-  background("#0a0a0f");
+  background("#74747d");
 
   if (!isPanning && (Math.abs(panVX) > 0.1 || Math.abs(panVY) > 0.1)) {
     panX += panVX;
